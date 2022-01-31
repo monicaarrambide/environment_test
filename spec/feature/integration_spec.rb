1c1895f74fa9f120093e9084f97539f55c944d02
monicaarrambide <55818711+monicaarrambide@users.noreply.github.com>
2022-01-30 20:11:08 -0600

    Wrote a few tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -3,19 +3,78 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
   # integration tests for title
-  scenario 'valid inputs' do
+  scenario 'valid inputs for title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
     click_on 'Create Book'
-    visit books_path
+    #visit books_path
     expect(page).to have_content('harry potter')
   end
 
-  scenario 'invalid inputs' do
+  scenario 'invalid inputs for title' do
     visit new_book_path
     fill_in 'Title', with: ''
     click_on 'Create Book'
     expect(page).to have_content("Title can't be blank")
+  end
+
+  #integration tests for author
+  scenario 'valid inputs for author' do
+    visit new_book_path
+    fill_in 'Author', with: 'J.K. Rowling'
+    click_on 'Create Book'
+    expect(page).to have_content('J.K. Rowling')
+  end
+
+  scenario 'invalid inputs for author' do
+    visit new_book_path
+    fill_in 'Author', with: ''
+    click_on 'Create Book'
+    expect(page).to have_content("Author can't be blank")
+  end
+
+  #integration tests for price
+  scenario 'valid inputs for price' do
+    visit new_book_path
+    fill_in 'Price', with: 5.00
+    click_on 'Create Book'
+    expect(page).to have_content(5.00)
+  end
+
+  scenario 'invalid inputs for price' do
+    visit new_book_path
+    fill_in 'Price', with: 0
+    click_on 'Create Book'
+    expect(page).to have_content("Book can't be free")
+  end
+
+  #integration tests for date
+  scenario 'valid inputs for date' do
+    visit new_book_path
+    fill_in 'Date', with: "%1/%30/%2022"
+    click_on 'Create Book'
+    expect(page).to have_content("%1/%30/%2022")
+  end
+
+  scenario 'invalid inputs for month' do
+    visit new_book_path
+    fill_in 'Date', with: "%13/%30/%2022"
+    click_on 'Create Book'
+    expect(page).to have_content("Book must have valid date")
+  end
+
+  scenario 'invalid inputs for day' do
+    visit new_book_path
+    fill_in 'Date', with: "%1/%35/%2022"
+    click_on 'Create Book'
+    expect(page).to have_content("Book must have valid date")
+  end
+
+  scenario 'invalid inputs for year' do
+    visit new_book_path
+    fill_in 'Date', with: "%1/%30/%2025"
+    click_on 'Create Book'
+    expect(page).to have_content("Book must have valid date")
   end
 end
 
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -10,8 +10,56 @@ RSpec.describe Book, type: :model do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without a title' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'J.K. Rowling')
+  end
+
+  it 'is valid with valid attributes - author' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+# unit test for price
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 5.00)
+  end
+
+  it 'is valid with valid attributes for price' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a proper price' do
+    subject.price = 5.015
+    expect(subject).not_to be_valid
+  end
+end
+
+# unit test for date
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 5.00, date: "%1/%30/%2022")
+  end
+
+  it 'is valid with valid attributes for date' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a proper date' do
+    subject.price = "%13/%20/%2022"
     expect(subject).not_to be_valid
   end
 end
