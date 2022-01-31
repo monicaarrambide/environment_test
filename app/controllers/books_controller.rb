4a5392c585a2523ae7b57cb05bb4a798555e0528
monicaarrambide <55818711+monicaarrambide@users.noreply.github.com>
2022-01-30 22:10:16 -0600

    Included author on Show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
 
   # GET /books/1 or /books/1.json
   def show
-    # @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   # GET /books/new
